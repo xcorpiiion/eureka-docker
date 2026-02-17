fbdc40547f628e12a27f56da98e9bdc0b4f45e00
xcorpiion <vinicius.da.silva.cruz.sanji@gmail.com>
2026-02-16 21:58:44 -0300

    initialize Eureka Docker application with Maven wrapper and configuration

---
+++ src/test/java/br/com/docker/eureka/eurekadocker/EurekaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.docker.eureka.eurekadocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
